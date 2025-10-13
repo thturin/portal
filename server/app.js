c778c6317ef16a9de26b854a74c31fd1609c5847
tatiana <tatiana.turin@gmail.com>
2025-10-13 16:07:11 -0400

    figuring out docker. added static url for docker in loginbar.jsx

--- server/app.js
+++ server/app.js
@@ -17,7 +17,11 @@ const prisma = new PrismaClient();
 
 //you are already using an authentication method so you ca * origin accept any 
 app.use(cors({
-  origin: process.env.CLIENT_URL, //there was no origin header
+  origin: [
+     process.env.CLIENT_URL, // e.g., http://localhost:13000 for local dev
+    'http://localhost:13000' // Docker Compose service name for frontend
+  
+  ],
   credentials:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
