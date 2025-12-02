8b076ae0964cadbfe87ac952d79d3d668cb20bd7
tatiana <tatiana.turin@gmail.com>
2025-12-02 12:19:37 -0500

    added node_env in app.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  // keep warn/error if you still want to see issues
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
