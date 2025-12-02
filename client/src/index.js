aa191b5c0639b137748d5680bb91d414ecca4a97
tatiana <tatiana.turin@gmail.com>
2025-12-02 14:43:06 -0500

    import axios

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 // import reportWebVitals from './reportWebVitals';
 
 
